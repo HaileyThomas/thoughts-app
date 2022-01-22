95662bf1842cded714bf7bb0dcd7672e5929cb44
Hailey Thomas <haileyraebeauty@gmail.com>
2022-01-22 15:39:03 -0700

    add reaction controllers

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -91,6 +91,40 @@ const thoughtController = {
       })
       .catch((err) => res.status(500).json(err));
   },
+
+  // add a reaction
+  addReaction({ params, body }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.thoughtId },
+      { $addToSet: { reactions: body } },
+      { new: true, runValidators: true }
+    )
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: 'No thought found with this id!' });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // delete a reaction
+  deleteReaction({ params, body }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.thoughtId },
+      { $pull: { reactions: { reactionId: body.reactionId } } },
+      { new: true, runValidators: true }
+    )
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: 'No thought found with this id' });
+          return;
+        }
+        res.json({ message: 'Successfully deleted the reaction' });
+      })
+      .catch((err) => res.status(500).json(err));
+  },
 };
 
 module.exports = thoughtController;
